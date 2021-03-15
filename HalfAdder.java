6a3071839c2510ed10fb45cee0281ee6a1d5f313
5aur4bh <80582242+5aur4bh@users.noreply.github.com>
2021-03-15 21:39:43 +0530

    Update HalfAdder.java

--- HalfAdder.java
+++ HalfAdder.java
@@ -1,17 +1,1 @@
-package com.assignment1;
 
-import java.util.Scanner;
-
-public class HalfAdder {
-    //Implement half-adder using Bitwise Operator
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        byte A = sc.nextByte();
-        byte B = sc.nextByte();
-
-        byte sum = (byte) (A&B);
-        
-
-
-    }
-}
